298c3ce42c529aa69d610a8b34c3d5c4b7549ced
Guilherme <guilherme1067@hotmail.com>
2023-02-28 18:06:15 -0300

    altered title for aria label

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -45,12 +45,12 @@ function HeaderSocialMediaIcons({ setMenuStatus }: MobileMenuProps) {
     <ul className="flex items-center sm:flex sm:gap-6">
       <li className='group '>
         <img onClick={() => setMenuStatus(true)} className="inline sm:hidden cursor-pointer" src={menu} />
-        <a title="Link to my linkeding perfil(external link)" className="hidden md:inline" href="https://www.linkedin.com/in/guireiss/" target="_blank">
+        <a aria-label="Link to my linkeding perfil(external link)" className="hidden md:inline" href="https://www.linkedin.com/in/guireiss/" target="_blank">
           <img src={linkedinIcon} alt="linkedin logo" />
         </a>
       </li>
-      <li><a title="Link to my github perfil(external link)" className="hidden md:inline" href="https://github.com/Guilherme1067" target="_blank"><img alt="github logo" src={githubIcon} /></a></li>
-      <li><a title="Link to my instagram perfil(external link)" className="hidden md:inline" href="https://www.instagram.com/guilhermedrs_/" target="_blank"><img src={facebookIcon} alt="instagram logo" /></a></li>
+      <li><a aria-label="Link to my github perfil(external link)" className="hidden md:inline" href="https://github.com/Guilherme1067" target="_blank"><img alt="github logo" src={githubIcon} /></a></li>
+      <li><a aria-label="Link to my instagram perfil(external link)" className="hidden md:inline" href="https://www.instagram.com/guilhermedrs_/" target="_blank"><img src={facebookIcon} alt="instagram logo" /></a></li>
     </ul>
   )
 }
